c7d9c2d0db7818c948f8e46b1201324545f2dd31
Pablo Sazo <pablo@glooadvertising.com.au>
2023-12-15 13:48:42 +1100

    Friday

--- index.php
+++ index.php
@@ -4,6 +4,8 @@
     <?php is_page('home') ? get_template_part( 'partials/featured-projects' ) : null ; ?>
     <?php is_page('home') ? get_template_part( 'partials/our-clients' ) : null ; ?>
     <?php is_page('about-us') ? get_template_part( 'partials/about' ) : null; ?>
+    <?php is_page('about-us') ? get_template_part( 'partials/promise' ) : null; ?>
+    <?php is_page('about-us') ? get_template_part( 'partials/award-winners' ) : null; ?>
     <?php is_page('our-process') ? get_template_part( 'partials/process' ) : null; ?>
     <?php is_page('our-projects') ? get_template_part( 'partials/projects' ) : null; ?>
 
---
+++ partials/promise.php
@@ -0,0 +1,31 @@
+<section class="promise" style="background-image: url('<?php echo get_template_directory_uri(); ?>/assets/images/placeholder.jpg')">
+    <div class="promise-white-block">
+        <div class="container">
+            <span>
+                <div class="col">
+                    <h3>Brand promise</h3>
+                    <p>Alterations:</p>
+                    <ul>
+                        <li>That meet your needs</li>
+                        <li>With clear and concise estimate</li>
+                        <li>With Quality Guaranteed</li>
+                    </ul>
+                </div>
+                <div class="col">
+                    <h3>Core values</h3>
+                    <ul>
+                        <li>Getting the job right the first time</li>
+                        <li>Clients first- we do the right thing by our clients</li>
+                        <li>Open and transparent communication</li>
+                        <li>Commitment to high quality service and products</li>
+                        <li>Staying true to our word and doing what we say we will do</li>
+                        <li>Maintaining a clean safe and tidy</li>
+                    </ul>
+                </div>
+            </span>
+            <div class="col">
+                <a href="/#contact-us" class="btn">Contact us</a>
+            </div>
+        </div>
+    </div>
+</section>
