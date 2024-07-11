52ea8b19fd5933b88146cf30d2c9871f89b5bbac
Pablo Sazo <pablo@glooadvertising.com.au>
2024-07-11 13:09:03 +1000

    facebook

--- footer.php
+++ footer.php
@@ -7,6 +7,12 @@
                 <span>
                     <p>m <a href="tel:0418-146-691">0418 146 691</a> p <a href="tel:03-6229-9930">03 6229 9930</a></p>
                     <p>e <a href="mailto:rob@blythewatchorn.com.au">rob@blythewatchorn.com.au</a></p>
+                    <br>
+                    <p>
+                        <a href="https://www.facebook.com/people/Blythe-Watchorn-Builders/61561604164876/" target="_blank" style="display: flex; align-items: center;">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" style="fill: 062752;transform: ;msFilter:;" class="facebook-icon"><path d="M12.001 2.002c-5.522 0-9.999 4.477-9.999 9.999 0 4.99 3.656 9.126 8.437 9.879v-6.988h-2.54v-2.891h2.54V9.798c0-2.508 1.493-3.891 3.776-3.891 1.094 0 2.24.195 2.24.195v2.459h-1.264c-1.24 0-1.628.772-1.628 1.563v1.875h2.771l-.443 2.891h-2.328v6.988C18.344 21.129 22 16.992 22 12.001c0-5.522-4.477-9.999-9.999-9.999z"></path></svg>&nbsp; Follow us on Facebook
+                        </a>
+                    </p>
                 </span>
                 <p>Copyright © <?php bloginfo( 'title' )?> - Site by <a href="https://glooadvertising.com.au">Gloo</a></p>
 
